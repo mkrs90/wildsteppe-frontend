590cf0e7bb5b8bba02b4b5c22afc3910afbe752a
mkrs90 <katy.simpson@student.awesomeinc.org>
2023-04-18 15:07:46 +0000

    set up login and logout plus users can now create an account

--- client/src/components/SiteNavBar.js
+++ client/src/components/SiteNavBar.js
@@ -1,9 +1,25 @@
+import { useGlobalState } from '../context/GlobalState';
 import SiteLogo from '../Images/wildsteppe_logo.png';
 import SiteName from '../Images/Wildsteppe_name.png';
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import AuthService from '../services/auth.service';
 
 function SiteNavBar() {
     
+    const [ state, dispatch ] = useGlobalState();
+    let navigate = useNavigate();
+    
+    const handleLogout = (e) => {
+        e.preventDefault();
+        AuthService.logout();
+        console.log("user clicked log out")
+        navigate('/');
+        dispatch({
+            currentUserToken: null,
+            currentUser: null
+        })
+    }
+
     return (
         <div id="navbar" className='navigation border border-bottom'>
             <nav className='navbar navbar-expand'>
@@ -15,7 +31,15 @@ function SiteNavBar() {
                     <ul className="navbar-nav ml-auto">
                         <Link to="/" id='navBarItems' className='nav-item me-5 mt-3'>Home</Link>
                         <Link to="/about" id='navBarItems' className='nav-item me-5 mt-3'>About Us</Link>
-                        <Link to="/login" id="loginButton" className='nav-item btn me-5'>Sign In</Link>
+                        {!state.currentUser &&
+                            <Link to="/login" id="loginButton" className='nav-item btn me-5'>Sign In</Link>
+                        }
+                        {state.currentUser && 
+                            <>
+                                <Link to="/profile" id='navBarItems' className='nav-item me-5 mt-3'>Profile</Link>
+                                <Link onClick={handleLogout} id="loginButton" className="nav-item btn me-5">Sign Out</Link>
+                            </>
+                        } 
                     </ul>
                 </div>                
             </nav>

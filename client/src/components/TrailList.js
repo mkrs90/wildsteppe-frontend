39878cc6958877d38b7635c10110354b54874dad
mkrs90 <katy.simpson@student.awesomeinc.org>
2023-04-19 20:25:50 +0000

    set up trail buttons to dynamically render

--- client/src/components/TrailList.js
+++ client/src/components/TrailList.js
@@ -1,61 +1,64 @@
 import { useState, useEffect } from "react";
-import axios from 'axios';
-import TrailPage from "../pages/TrailPage";
+import axios from "axios";
+import { NavLink, useNavigate } from "react-router-dom";
+import trailPhoto from '../Images/trail_test_photo.jpg';
 
-const BASE_URL = 'https://8000-mkrs90-wildsteppebacken-vpz7io4uhwz.ws-us94.gitpod.io/api';
+const BASE_URL =
+  "https://8000-mkrs90-wildsteppebacken-vpz7io4uhwz.ws-us94.gitpod.io/api";
 
 function TrailList() {
-    const [trails, setTrails] = useState([]);
+  const [trails, setTrails] = useState([]);
 
-    useEffect(() => {
-        const getTrails = async () => {
-            let config = {
-                url: '/trails/',
-                baseURL: BASE_URL,
-                method: 'get'
-            }
-            let response = await axios.request(config);
-            const limitedData = response.data.slice(0, 3);
-            setTrails(limitedData);
-        }
-        getTrails();
-    }, [])
+  useEffect(() => {
+    const getTrails = async () => {
+      let config = {
+        url: "/trails/",
+        baseURL: BASE_URL,
+        method: "get",
+      };
+      let response = await axios.request(config);
+      const limitedData = response.data.slice(0, 3);
+      setTrails(limitedData);
+    };
+    getTrails();
+  }, []);
 
-
-    return (
-        <div className="container-fluid">
-            <div className="container mt-5">
-                <div id="trail_list_header" className="border-bottom">
-                    <p id="trail_list_title" className="">Trails Near You</p>
-                    <p className="float-end float-top">See More...</p>
-                </div>
-            </div>
-            <div className="container mt-3">
-                <div className="row justify-content-evenly p-2">
-                    {trails.map(trail => {
-                    return <TrailCard trail={trail}/>
-                    })}
-                </div>
-            </div>
-            
+  return (
+    <div className="container-fluid">
+      <div className="container mt-5">
+        <div id="trail_list_header" className="border-bottom">
+          <p id="trail_list_title" className="">
+            Trails Near You
+          </p>
+          <p className="float-end float-top">See More...</p>
         </div>
-    )
+      </div>
+      <div className="container mt-3">
+        <div className="row justify-content-evenly p-2">
+          {trails.map((trail) => (
+            <TrailCard key={trail.id} trail={trail} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export const TrailCard = ({ trail }) => {
-    return (
-        <>
-            <div className="card text-center" style={{width: '18rem'}}>
-                <img src="..." className="card-img-top" alt="..." />
-                    <div className="card-body">
-                        <h5 className="card-title">{trail.name}</h5>
-                        {/* <p className="card-text"></p> */}
-                        <button id="loginButton" className="btn" href={<TrailPage trail_id={trail.id}/>}>See the Trail</button>
-                        {/* <button href="/trailPage" id="loginButton" className="btn">See the Trail</button> */}
-                    </div>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="card" style={{ width: "18rem" }}>
+        <img src={trailPhoto} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{trail.name}</h5>
+          {/* <p className="card-text"></p> */}
+          <NavLink to={`/trail/${trail.id}`} id="loginButton" className="btn">
+            See the Trail
+          </NavLink>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default TrailList;
--- client/src/pages/TrailPage.js
+++ client/src/pages/TrailPage.js
@@ -1,58 +1,65 @@
 import { useState, useEffect } from "react";
-import axios from 'axios';
+import axios from "axios";
+import { useParams } from "react-router-dom";
 // import { API_URL, TRAIL_ENDPOINT } from "../services/auth.constants";
 
-const BASE_URL = 'https://8000-mkrs90-wildsteppebacken-vpz7io4uhwz.ws-us94.gitpod.io/api';
+const BASE_URL =
+  "https://8000-mkrs90-wildsteppebacken-vpz7io4uhwz.ws-us94.gitpod.io/api";
 
-export default function TrailPage({ trail_id }) {
-    const [trailDetails, setTrailDetails] = useState([]);
+export default function TrailPage() {
+  const { id } = useParams();
+  const [trailDetails, setTrailDetails] = useState([]);
+  useEffect(() => {
+    const getTrailDetails = async () => {
+      let config = {
+        url: `/trails/${id}`,
+        baseURL: BASE_URL,
+        method: "get",
+      };
+      let response = await axios.request(config);
+      console.log(response);
+      setTrailDetails(response.data);
+    };
+    getTrailDetails();
+  }, [id]);
 
-    useEffect(() => {
-        const getTrailDetails = async () => {
-            let config = {
-                url: '/trails/',
-                baseURL: BASE_URL,
-                method: 'get'
-            }
-            let response = await axios.request(config);
-            console.log(response);
-            setTrailDetails(response.data);
-        }
-        getTrailDetails();
-    }, [])
-
-
-    return (
-        <div className="container m-5">
-            <div>
-                {trailDetails.filter(trail => trail.id === {trail_id}).map(trail => (
-                        <TrailDetailCard key={trail.id} trail={trail} />
-                ))}
-            </div>
-        </div>
-    )
+  return (
+    <div id="trailPage_detailsCard" className="container m-5 border border-dark p-2">
+      <div>
+        <TrailDetailCard key={trailDetails.id} trail={trailDetails} />
+      </div>
+    </div>
+  );
 }
 
 export const TrailDetailCard = ({ trail }) => {
-    return (
-        <>
-            <div className="container p-2" id="trailPage_Card">
-                <div id="trailPage_header" className="position-absolute bottom-50 start-0 ms-5">
-                        <div id="trail_title" className="ms-5">{trail.name}</div>
-                        <div id="trail_title_subtext" className="ms-5">
-                            <div>Trial Rating</div>
-                            <div>Difficulty: Easy</div>
-                            <div>{trail.location}</div>
-                        </div>
-                        
-                </div>
-                
-            </div>
-            <div>
-                <p>{trail.description}</p>
-                <div>{trail.distance}</div>
-                <div>{trail.duration}</div>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="container-fluid" id="trailPage_Card">
+        <div
+          id="trailPage_header"
+          className="position-absolute bottom-50 start-0 ms-5"
+        >
+          <div id="trail_title" className="ms-5">
+            {trail.name}
+          </div>
+          <div id="trail_title_subtext" className="ms-5">
+            <div>Trial Rating</div>
+            <div>Difficulty: Easy</div>
+            <div>{trail.location}</div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div id="trailPage_description" className="mt-5 p-2">
+          <div className="border-bottom">Description</div>
+          <p className="mt-2">{trail.description}</p>
+        </div>
+        <div>
+          <div>Distance: {trail.distance}miles</div>
+          <div>Estimated Duration: {trail.duration}hrs</div>
+        </div>
+      </div>
+    </>
+  );
+};

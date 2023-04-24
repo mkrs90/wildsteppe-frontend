7d7eb38c83e4327953084d14da1b20987b0a6113
mkrs90 <katy.simpson@student.awesomeinc.org>
2023-04-24 20:39:11 +0000

    got star rating and basic comment style finished

--- client/src/components/ViewComments.js
+++ client/src/components/ViewComments.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { API_URL, COMMENT_ENDPOINT } from "../services/auth.constants";
 import axios from "axios";
+import birdIcon from "../Images/bird_icon.png";
 
 function ViewComments({ trailId }) {
     const [reviews, setReviews] = useState([]);
@@ -20,7 +21,6 @@ function ViewComments({ trailId }) {
 
     return (
         <div>
-            Here are the comments!
             <div>
                 {reviews
                 .filter(r => r.trail.id === trailId)
@@ -31,13 +31,39 @@ function ViewComments({ trailId }) {
 }
 
 export const CommentCard = ({ review }) => {
+    const formatDate = (rd) => {
+        const date = new Date(rd);
+        const monthNames =["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        const formattedDate = `${monthNames[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
+        return formattedDate;
+    }
+
+    const createStars = (sr) => {
+        const stars = [];
+        for (let i = 0; i < sr; i++) {
+            stars.push(<i className="fa fa-solid fa-star" id="rating_star"></i>)
+        }
+        return stars;
+    }
+    // <i class="fa-solid fa-star" style="color: #1d4790;"></i>
+
     return (
-        <>
-            <div>user {review.user.first_name}</div>
-            <div>Date Visited {review.date}</div>
-            <div>Rating {review.stars}</div>
-            <div>Review: {review.text}</div>
-        </>
+        <div id="comment_container" className="border-bottom">
+            <div className="row mt-3">
+                <div className="col-1 ms-3 mt-1">
+                    <img src={birdIcon} alt="icon" id="userImg" />
+                </div>
+                <div className="col-10 float-start">
+                    <div id="comment_textBold">{review.user.first_name}</div>
+                    <div>{formatDate(review.date)}</div>
+                </div>
+            </div>
+            <div className="row">
+               <div id="comment_stars" className="ms-3">{createStars(review.stars)}</div> 
+            </div>
+            
+            <div id="comment_text" className="ms-3">{review.text}</div>
+        </div>
     )
 }
 
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -14,3 +14,5 @@ const Profile = () => {
 }
 
 export default Profile;
+
+
